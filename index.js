ebb32c98e6b118a0b363dd31eb2d58db7fbdf41a
zocky <zocky@naoknu.(none)>
2013-06-18 19:17:42 +0200

    + .about

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ var bot = {
       if (m) {
         if(m[1] == '.') {
           me.doMessage(from,m[2].trim(),function(reply) {
-            console.log('respond',reply);
             me.say(reply);
           });
         } else {
@@ -108,7 +107,6 @@ var bot = {
   _print: function(nick,text) {
 
     text = String(text).clean().shorten(430);
-    console.log('print',nick,text);
     var pending = this.pending[nick];
     switch(pending[pending.length-1]) {
     case '':
@@ -240,7 +238,7 @@ require('./modules/w.js').setup(bot);
 
 
 bot.addCommand('help', {
-  usage: 'help, help [command]',
+  usage: '.help, .help [command]',
   help: "dumbfuck: n. somebody who can't figure out what .help does",
   action: function(from,respond,text,cmd) {
     if (!cmd) return respond.flush(
@@ -256,6 +254,19 @@ bot.addCommand('help', {
     respond.flush(bot.commands[cmd].usage + ' | ' + bot.commands[cmd].help,'<br>','for more help, see a psychiatrist');
   }
 })
+
+bot.addCommand('about', {
+  usage: '.about',
+  help: "what about .about?",
+  action: function(from,respond,text,cmd) {
+    respond.print("I'm a langbot bot for " + bot.config.channel,'|');
+    respond.print(bot.config.master ? 'My master is '+bot.config.master + '.': 'I have no master.', "|");
+    respond.print("If you kick me, I won't come back unless you /invite me.",'|');
+    respond.print("Source, docs, bug reports and feature requests at: https://github.com/zocky/langbot");
+    respond.flush();
+  }
+})
+
 
 bot.addCommand('more', {
   usage: '.more',

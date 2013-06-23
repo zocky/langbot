7183df3dedf7244a6c7f76b965536e9003472fba
zocky <zocky@naoknu.(none)>
2013-06-23 03:10:05 +0200

    new ipa2x, x2ipa

--- src/modules/ipa.mod.js
+++ src/modules/ipa.mod.js
@@ -1,23 +1,94 @@
 var data = require('./ipa.data.js');
 
-exports.setup = function(bot) {
+  var _trans = {};
+  for (var name in data.trans) {
+    var t = data.trans[name];
+    var _t = _trans[name] = {};
+    var ipa = Object.keys(t);
+    ipa.sort(function(a,b){return b.length-a.length});
+    _t.ipa2x_re = new RegExp('(' + ipa.filter(Boolean).map(RegExp.escape).join('|') + ')');
+    _t.ipa2x = {}; for(var i in t) _t.ipa2x[i] = t[i].split(/\|/)[0];
+    var x = []; for(var i in ipa) x=x.concat((t[ipa[i]]||'').split(/\|/));
+    _t.x2ipa_re = new RegExp('(' + x.sort(function(a,b){return b.length-a.length}).map(RegExp.escape).join('|') + ')');
+    _t.x2ipa = {}; for(var i in t) t[i].split(/\|/).filter(Boolean).forEach(function(n){_t.x2ipa[n] = i});
+ }
+ 
+ console.log(_trans.locaphone);
+ var ipa2x = function fn (name,str) {
+    var t = _trans[name];
+    return ('/'+str+'/')
+    .replace(/[/]+/g,'/')
+    .split(t.ipa2x_re)
+    .filter(Boolean)
+    .map(function(n){return n in t.ipa2x ? t.ipa2x[n] : n})
+    .join('');
+  }
+  
+  var x2ipa = function fn (t,str) {
+    var t = _trans[name];
+    return ('/'+str+'/')
+    .replace(/[/]+/g,'/')
+    .split(t.x2ipa_re)
+    .filter(Boolean)
+    .map(function(n){return n in t.x2ipa ? t.x2ipa[n] : n})
+    .join('');
+  }
 
-  bot.addCommand('xsampa', {
-    usage: '.xsampa [text]',
-    help: 'Convert IPA to X-SAMPA',
+exports.setup = function(bot) {
+  bot.addCommand('x', {
+    usage: '.x [text]',
+    help: 'Convert X-SAMPA to IPA and locaphone',
     args: /(.+)$/,
     action: function(from,respond,text) {
-      ipa2xsampa(from,respond,text)
+      var ipa = x2ipa('xsampa',text);
+      var lcp = ipa2x('locaphone',ipa);
+      respond.flush('ipa:'+ipa, 'lcp:'+lcp);
     }
   })
 
-  function ipa2xsampa(from,respond,text) {
-    var newtext = [];
-    for(i=0; i<text.length; i++) {
-	newtext[i] = data.ipa2xsampa[text[i]] || text[i];
+  bot.addCommand('l', {
+    usage: '.l [text]',
+    help: 'Convert locaphone to IPA and X-SAMPA',
+    args: /(.+)$/,
+    action: function(from,respond,text) {
+      var ipa = x2ipa('locaphone',text);
+      var xsampa = ipa2x('xsampa',ipa);
+      respond.flush('ipa:'+ipa,'x-sampa:'+xsampa);
     }
-    respond.print(newtext.join(''));
-    respond.flush();
-  };
+  })
+
+  bot.addCommand('i', {
+    usage: '.i [text]',
+    help: 'Convert IPA locaphone and X-SAMPA',
+    args: /(.+)$/,
+    action: function(from,respond,ipa) {
+      var lcp = ipa2x('locaphone',ipa);
+      var xsampa = ipa2x('xsampa',ipa);
+      respond.flush('x-sampa:'+xsampa,'lcp:'+lcp);
+    }
+  })
+
+  bot.addCommand('pron', {
+    usage: '.pron [word]',
+    help: 'display pronunciation of a word',
+    args: /^(.+)$/,
+    action: function(from,respond,text) {
+      bot.wget('http://oxforddictionaries.com/search/english/?direct=1&multi=1', {
+        q:text
+      }, function(error,response,body,url) {
+        if (error) return respond('error',String(error));
+        var ipa = body.extract(/<a href="http:..oxforddictionaries.com.words.key-to-pronunciation">\s*(\/.+?\/)\s*<\/a>/i,'$1');
+        if (!ipa) return respond('nothing found');
+        var lcp = ipa2x('locaphone',ipa);
+        var xsampa = ipa2x('xsampa',ipa);
+        respond.flush('ipa:'+ipa,' x-sampa:'+xsampa,'lcp:'+lcp);
+      });
+    }
+  })
+
 
 }
+
+
+  
+ 

4143930404b0937e83049f53c17c1ec92ed1d80a
zocky <zocky@nospam.please>
2015-04-09 00:34:42 +0200

    really replace google translate with frengly

--- src/modules/chat.mod.js
+++
@@ -1,9 +0,0 @@
-exports.setup = function(bot) {
-  bot.state.seen =  bot.state.seen || {};
-
-  bot.on('talk',function (from, message) {
-    if (Math.random()<0.25) {
-      bot.say(from+', wtf?');
-    }
-  });
-}
